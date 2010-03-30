3b0749357092f6b50c191969742bde67a5249615
Jonas Bjork <jonas@jonasbjork.net>
2010-03-30 12:16:23 +0200

    starting to build the plugin support

---
+++ plugins/popularpostsdata.plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+postid, day, pageviews
+CREATE TABLE `asa_popularpostsdata` (
+  `postid` int(10) NOT NULL,
+  `day` datetime NOT NULL default '0000-00-00 00:00:00',
+  `pageviews` int(10) default '1',
+  UNIQUE KEY `id` (`postid`,`day`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+
+
+
+
+
